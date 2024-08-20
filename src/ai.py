4fac509a29138d64a3e9fed0667788cbdb66329c
sheepskins <mitchrosser13@gmail.com>
2024-08-20 12:22:59 +1000

    move_to client

--- src/ai.py
+++ src/ai.py
@@ -2,6 +2,9 @@ import detection.detector as detector
 import rospy
 import pandas as pd
 from spottyai.srv import detection, detectionResponse
+from spot_msgs.msg import TrajectoryAction, TrajectoryResult, TrajectoryFeedback, TrajectoryGoal
+from std_msgs.msg import Duration
+import actionlib
 
 def detection_client(category):
     rospy.wait_for_service('detection_service')
@@ -16,6 +19,28 @@ def detection_client(category):
     except rospy.ServiceException as e:
         rospy.logerr(f"Service call failed: {e}")
         return None
+
+def move_to(x,y):
+    client = actionlib.SimpleActionClient('trajectory', TrajectoryAction)
+    
+    client.wait_for_server()
+
+    goal = TrajectoryGoal()
+
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.header.frame_id = 'body'
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y
+    goal.duration = rospy.Duration(120)
+
+    goal.precise_positioning = 0
+
+    client.send_goal(goal)
+
+    client.wait_for_result()
+
+    return client.get_result()
+
 
 def say(string):
     print(string)
